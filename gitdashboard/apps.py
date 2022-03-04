e1c38fa265d9f84fc85d93b23a7cde92b21ee2ef
Subhashini Sundaresan <ssubu28@gmail.com>
2022-03-04 01:50:43 -0500

    base - django setup

---
+++ gitdashboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GitdashboardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gitdashboard'
---
+++ gitdashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+import requests
+
+# Create your views here.
+
+def viewfunc1(request):
+    pass
